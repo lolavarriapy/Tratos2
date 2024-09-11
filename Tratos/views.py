e3b9bd4fd91538424b711fdaf4eede3a30bf55bf
lolavarriapy <lolavarria@py.cl>
2024-09-11 16:58:11 -0300

    fix: Corrección código de trato autogenerado
    Se agrega validación para que verifique el código de trato por obra y en estado activo

--- Tratos/views.py
+++ Tratos/views.py
@@ -104,7 +104,7 @@ def tratos_crear(request):
             oUnidadMedida = UnidadMedida.objects.get(id=idUnidadMedida)
 
             try:
-                ultimoTrato = Trato.objects.filter(categoria_id=oCategoria).latest("orden")
+                ultimoTrato = Trato.objects.filter(categoria_id=oCategoria,obra=oObra,estado=1).latest("orden")
             except ObjectDoesNotExist:
                 ultimoTrato = None
             
@@ -115,7 +115,7 @@ def tratos_crear(request):
             
             
             
-            vTrato = Trato.objects.filter(cod=icod).exists()
+            vTrato = Trato.objects.filter(cod=icod,obra=oObra,estado=1).exists()
             if vTrato:
                 unidad = {'id':'0','desc':'rep','rsp':'NOK'}
             else:
